fbc82a64d8c7c2606f71135334ba1ff81b0410a9
Karan Singh Guleriya <himekaranguleria@gmail.com>
2019-10-21 01:07:21 +0530

    Loaders are transformers

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+// Loders are transformers
+require('./main.css');
+
+
+// Buildig the module
 import notification from './Notification';
-//console.log(notification);
+// console.log(notification);
 notification.anounce("Its an anouncement");
 notification.log("Its an console log");
